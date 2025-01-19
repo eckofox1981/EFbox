62e3009ba03e3a6febec6c2e90489c0cba3536e2
eckofox1981 <franklineck@pm.me>
2025-01-19 19:53:17 +0100

    added ApiUser package and wrote variables for the different classes

--- src/main/java/eckofox/EFbox/ApiUser/ApiUserController.java
+++ src/main/java/eckofox/EFbox/ApiUser/ApiUserController.java
@@ -1,11 +1,15 @@
 package eckofox.EFbox.ApiUser;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
 public class ApiUserController {
     private final ApiUserService userservice;
 
+    @Autowired
+    public ApiUserController(ApiUserService userservice) {
+        this.userservice = userservice;
+    }
 }
--- src/main/java/eckofox/EFbox/ApiUser/ApiUserService.java
+++ src/main/java/eckofox/EFbox/ApiUser/ApiUserService.java
@@ -1,7 +1,14 @@
 package eckofox.EFbox.ApiUser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApiUserService {
+    private final ApiUserRepository userRepository;
+
+    @Autowired
+    public ApiUserService(ApiUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }

faa98949e7142fd9a03b0d1035ce3973a20e1400
eckofox1981 <franklineck@pm.me>
2025-03-06 10:56:30 +0100

    commenting

--- src/main/java/eckofox/EFbox/fileobjects/efboxfolder/EFBoxFolderController.java
+++ src/main/java/eckofox/EFbox/fileobjects/efboxfolder/EFBoxFolderController.java
@@ -22,9 +22,13 @@ public class EFBoxFolderController {
     private EFBoxFolderService folderService;
 
     /**
-     * all methods send to EFBoxFolderService which returns accordingly
+     * sends folder name to service for folder creation
+     * non-cacheable
      *
      * @param user will be used to check access rights in Service
+     * @param folderName sent to folder id
+     * @param parentFolderID to locate new folder location (0 if root)
+     * @return EntityModel with hypermedia or error message
      */
     @SuppressWarnings("DataFlowIssue")
     @PostMapping("/create")
@@ -60,6 +64,15 @@ public class EFBoxFolderController {
         }
     }
 
+    /**
+     * sends the folderID to service where user ownership is checked. Model is returned here then converted to
+     * EntityModel
+     * non-cacheable
+     *
+     * @param folderID of folder to be shown
+     * @param user to check ownership in service
+     * @return EntityModel with hypermedia or error message
+     */
     @GetMapping("/browse")
     public ResponseEntity<?> seeFolderContent(@RequestParam String folderID, @AuthenticationPrincipal User user) {
         try {
@@ -96,7 +109,7 @@ public class EFBoxFolderController {
      *
      * @param query a string trying to be matched in EFBoxFolderRepository and EFBoxFileRepository
      * @param user  used to filter out matches not belonging to the user
-     * @return ResponseEntity
+     * @return EntityModel with hypermedia or error message
      */
     @GetMapping("/search/{query}")
     public ResponseEntity<?> searchWithQuery(@PathVariable String query, @AuthenticationPrincipal User user) {
@@ -117,6 +130,13 @@ public class EFBoxFolderController {
         }
     }
 
+    /**
+     * sends folderID of folder to be deleted to service for deletion. User ownership checked in service
+     *
+     * @param folderID of folder to be deleted
+     * @param user to check ownership
+     * @return String message
+     */
     @DeleteMapping("/delete")
     public ResponseEntity<?> deleteFolder(@RequestParam String folderID, @AuthenticationPrincipal User user) {
         try {
@@ -131,6 +151,15 @@ public class EFBoxFolderController {
         }
     }
 
+    /**
+     * sends new folder name and folderID to be updated to service. User ownership is then checked before updating name
+     * non-cacheable
+     *
+     * @param user to check folder ownership
+     * @param folderID of folder to be updated
+     * @param newName of the folder
+     * @return EntityModel with hypermedia or error message
+     */
     @PutMapping("/change-name")
     public ResponseEntity<?> changeFolderName(@AuthenticationPrincipal User user, @RequestParam String folderID, @RequestParam String newName) {
         try {

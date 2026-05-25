3a816a838235b2dd134762b70603341d14e39c22
eckofox1981 <eckofox1981@pm.me>
2026-05-25 14:41:48 +0200

    fix: following sonarqube suggestion -> remove unused import

--- src/main/java/eckofox/efbox/exception/AccessCodeDoesNotExistsException.java
+++ src/main/java/eckofox/efbox/exception/AccessCodeDoesNotExistsException.java
@@ -1,7 +1,5 @@
 package eckofox.efbox.exception;
 
-import java.rmi.NoSuchObjectException;
-
 public class AccessCodeDoesNotExistsException extends Exception {
 
     public AccessCodeDoesNotExistsException(String s) {

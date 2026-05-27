074e7ca8b6ac7d556ba1ad559adc6432de70e2bf
eckofox1981 <eckofox1981@pm.me>
2026-05-27 15:09:33 +0200

    fix: refactor for readability. tested

--- src/main/java/eckofox/efbox/security/validation/filevalidation/detector/PdfDocumentDetector.java
+++ src/main/java/eckofox/efbox/security/validation/filevalidation/detector/PdfDocumentDetector.java
@@ -8,33 +8,41 @@ import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfReader;
 import eckofox.efbox.exception.FileValidationException;
 
+/**
+ *  from the https://github.com/righettod/document-upload-protection
+ *  checks the document using the itextpdf-library API for 'PDF'
+ */
 public class PdfDocumentDetector implements DocumentDetector {
     @Override
     public boolean isSafe(File f) {
         boolean safeState = false;
         try {
-            if ((f != null) && f.exists()) {
-                // Load stream in PDF parser
-                // If the stream is not a PDF then exception will be throwed
-                // here and safe state will be set to FALSE
-                PdfReader reader = new PdfReader(f.getAbsolutePath());
-                // Check 1:
-                // Detect if the document contains any JavaScript code
-                String jsCode = reader.getJavaScript();
-                if (jsCode == null) {
-                    // OK no JS code then when pass to check 2:
-                    // Detect if the document has any embedded files
-                    PdfDictionary root = reader.getCatalog();
-                    PdfDictionary names = root.getAsDict(PdfName.NAMES);
-                    PdfArray namesArray = null;
-                    if (names != null) {
-                        PdfDictionary embeddedFiles = names.getAsDict(PdfName.EMBEDDEDFILES);
-                        namesArray = embeddedFiles.getAsArray(PdfName.NAMES);
-                    }
-                    // Get safe state from number of embedded files
-                    safeState = ((namesArray == null) || namesArray.isEmpty());
-                }
+            if ((f == null) && !f.exists()) {
+                return false;
             }
+
+            // Load stream in PDF parser
+            // If the stream is not a PDF then exception will be thrown
+            // here and safe state will be set to FALSE
+            PdfReader reader = new PdfReader(f.getAbsolutePath());
+            // Check 1:
+            // Detect if the document contains any JavaScript code
+            String jsCode = reader.getJavaScript();
+
+            if (jsCode != null) {
+                return false;
+            }
+            // OK no JS code then when pass to check 2:
+            // Detect if the document has any embedded files
+            PdfDictionary root = reader.getCatalog();
+            PdfDictionary names = root.getAsDict(PdfName.NAMES);
+            PdfArray namesArray = null;
+            if (names != null) {
+                PdfDictionary embeddedFiles = names.getAsDict(PdfName.EMBEDDEDFILES);
+                namesArray = embeddedFiles.getAsArray(PdfName.NAMES);
+            }
+            // Get safe state from number of embedded files
+            safeState = ((namesArray == null) || namesArray.isEmpty());
         } catch (Exception e) {
             throw new FileValidationException("Error during Pdf file analysis:" + e);
         }

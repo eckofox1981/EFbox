7eade2d5479de9237aad97fb0e78d44864ab17f7
eckofox1981 <eckofox1981@pm.me>
2026-05-27 15:25:52 +0200

    fix: add emptystring check

--- src/main/java/eckofox/efbox/security/validation/filevalidation/detector/WordDocumentDetector.java
+++ src/main/java/eckofox/efbox/security/validation/filevalidation/detector/WordDocumentDetector.java
@@ -10,10 +10,13 @@ import com.aspose.words.NodeType;
 import com.aspose.words.Shape;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ *  from the https://github.com/righettod/document-upload-protection
+ *  checks the document using the aspose.com API for 'cells'
+ */
 public class WordDocumentDetector implements DocumentDetector {
     /**
      * List of allowed Word format (WML = Word ML (Word 2003 XML)).<br>
@@ -31,49 +34,56 @@ public class WordDocumentDetector implements DocumentDetector {
     @Override
     public boolean isSafe(File f) {
         boolean safeState = false;
-        //TODO: remove nested if and if and if
         try {
-            if ((f != null) && f.exists() && f.canRead()) {
-                // Perform a first check on Word document format
-                FileFormatInfo formatInfo = FileFormatUtil.detectFileFormat(f.getAbsolutePath());
-                String formatExtension = FileFormatUtil.loadFormatToExtension(formatInfo.getLoadFormat());
-
-                if ((formatExtension != null)
-                        && ALLOWED_FORMAT
-                        .contains(formatExtension.toLowerCase(Locale.US).replaceAll("\\.", ""))) {
-                    // Load the file into the Word document parser
-                    Document document = new Document(f.getAbsolutePath());
-                    // Get safe state from Macro presence
-                    safeState = !document.hasMacros();
-
-                    // If document is safe then we pass to OLE objects analysis
-                    if (safeState) {
-                        // Get all shapes of the document
-                        NodeCollection shapes = document.getChildNodes(NodeType.SHAPE, true);
-                        Shape shape = null;
-                        // Search OLE objects in all shapes
-                        int totalOLEObjectCount = 0;
-                        for (int i = 0; i < shapes.getCount(); i++) {
-                            shape = (Shape) shapes.get(i);
-                            // Check if the current shape has OLE object
-                            if (shape.getOleFormat() != null) {
-                                totalOLEObjectCount++;
-                            }
-                        }
-
-                        // Update safe status flag according to number of OLE object found
-                        if (totalOLEObjectCount != 0) {
-                            safeState = false;
-                        }
-
-                    }
-                }
+            if ((f == null) && !f.exists() && !f.canRead()) {
+                return false;
             }
-        }
-        catch (Exception e) {
-            safeState = false;
+
+            // Perform a first check on Word document format
+            FileFormatInfo formatInfo = FileFormatUtil.detectFileFormat(f.getAbsolutePath());
+            String formatExtension = FileFormatUtil.loadFormatToExtension(formatInfo.getLoadFormat());
+
+            if ((formatExtension == null || formatExtension.isBlank())
+                    && !ALLOWED_FORMAT
+                    .contains(formatExtension.toLowerCase(Locale.US).replaceAll("\\.", ""))) {
+                return false;
+            }
+            // Load the file into the Word document parser
+            Document document = new Document(f.getAbsolutePath());
+            // Get safe state from Macro presence
+            safeState = !document.hasMacros();
+
+            // If document is safe then we pass to OLE objects analysis
+            if (safeState) {
+                safeState = oleCheck(document);
+            }
+        } catch (Exception e) {
             throw new FileValidationException("\"Error during Word file analysis: " + e.getMessage());
         }
         return safeState;
+    }
+
+    /**
+     * check the word-doc's shapes for OLE-objects using the aspose API.
+     * if none are found returns true else false
+     * @param document
+     * @return
+     */
+    private boolean oleCheck(Document document) {
+        // Get all shapes of the document
+        NodeCollection shapes = document.getChildNodes(NodeType.SHAPE, true);
+        Shape shape = null;
+        // Search OLE objects in all shapes
+        int totalOLEObjectCount = 0;
+        for (int i = 0; i < shapes.getCount(); i++) {
+            shape = (Shape) shapes.get(i);
+            // Check if the current shape has OLE object
+            if (shape.getOleFormat() != null) {
+                totalOLEObjectCount++;
+            }
+        }
+
+        // Update safe status flag according to number of OLE object found
+        return totalOLEObjectCount == 0;
     }
 }

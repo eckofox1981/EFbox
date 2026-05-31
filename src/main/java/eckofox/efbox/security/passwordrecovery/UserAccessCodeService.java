6e4c962d440636ee5e90ee80e0863c0a69588152
eckofox1981 <eckofox1981@pm.me>
2026-05-31 13:47:42 +0200

    docs: comment UserAccessCodeService package

--- src/main/java/eckofox/efbox/security/passwordrecovery/UserAccessCodeService.java
+++ src/main/java/eckofox/efbox/security/passwordrecovery/UserAccessCodeService.java
@@ -18,6 +18,13 @@ public class UserAccessCodeService {
     private final UserAccessCodeRepository accessCodeRepository;
     private final SecureRandom secureRandom;
 
+    /**
+     * generate a code linked to a user for password recovery
+     * checks if a request has already been made
+     * @param purpose for EmailType
+     * @param user supposedly making the request
+     * @return code in Integer
+     */
     public int generateAndSaveCode(EmailType purpose, User user) {
         //OWASP
         //https://cheatsheetseries.owasp.org/cheatsheets/Cryptographic_Storage_Cheat_Sheet.html#secure-random-number-generation
@@ -37,6 +44,14 @@ public class UserAccessCodeService {
         return code;
     }
 
+    /**
+     * controls the username making the request has the corresponding code in the database
+     * @param code given by requesting user
+     * @param user requesting User
+     * @param purpose EmailType for database comparison
+     * @return boolean if test passes
+     * @throws AccessCodeDoesNotExistsException
+     */
     public boolean checkCodeValidity(int code, User user, EmailType purpose) throws AccessCodeDoesNotExistsException {
         UserAccessCode existingRequest =
                 accessCodeRepository.findByUserIDAndPurpose(user.getUserID(), purpose).orElse(null);
@@ -59,6 +74,12 @@ public class UserAccessCodeService {
         return true;
     }
 
+    /**
+     * controls database to avoid multiple password request, deletes an old one to avoid storing multiple one
+     * the idea is that a user might lose the sent code and might need a new one.
+     * @param purpose EMailType to check if similar request have been made
+     * @param user linked to type of requests
+     */
     private void ifRequestAlreadyExistingDelete(EmailType purpose, User user) {
         UserAccessCode existingRequest = accessCodeRepository
                 .findByUserIDAndPurpose(user.getUserID(), purpose).orElse(null);

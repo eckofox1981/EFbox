4f8412c7ccc7e248f20e31aa058545d4964eed3c
eckofox1981 <eckofox1981@pm.me>
2026-07-23 05:05:20 +0200

    feat: adapted DTO for practical frontend handling (change from proof of concept only display name)

--- src/main/java/eckofox/efbox/fileobjects/efboxfolder/EFBoxFolderDTO.java
+++ src/main/java/eckofox/efbox/fileobjects/efboxfolder/EFBoxFolderDTO.java
@@ -4,16 +4,19 @@ import eckofox.efbox.fileobjects.efboxfile.EFBoxFile;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Data
 public class EFBoxFolderDTO {
     private UUID folderID;
     private String name;
-    private List<String> folderNames;
-    private List<String> fileNames;
+    private String parentID;
+    private List<UUID> folderIDs;
+    private HashMap<UUID, String> fileHashes;
 
     /**
      * converts model to DTO
@@ -22,17 +25,22 @@ public class EFBoxFolderDTO {
      * @return folder dto
      */
     public static EFBoxFolderDTO fromEFBoxFolder(EFBoxFolder folder) {
+        EFBoxFolder parent = folder.getParentFolder();
+
         return new EFBoxFolderDTO(
                 folder.getFolderID(),
                 folder.getName(),
+                parent == null ? "" : parent.getFolderID().toString(),
                 folder.getFolders()
                         .stream()
-                        .map(EFBoxFolder::getName)
+                        .map(EFBoxFolder::getFolderID)
                         .toList(),
-                folder.getFiles()
+                (HashMap<UUID, String>) folder.getFiles()
                         .stream()
-                        .map(EFBoxFile::getFileName)
-                        .toList()
+                        .collect(Collectors.toMap(
+                                EFBoxFile::getFileID,
+                                EFBoxFile::getFileName
+                        ))
         );
     }
 }
